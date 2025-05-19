81365c7fe4ee385b745134e88d0a981c698270d5
Lubin Mendez <mendezlubin@yahoo.com>
2025-05-19 11:01:01 -0700

    Set CSS variable --vh via JavaScript to fix 100vh scroll issue on mobile devices

--- js/code.js
+++ js/code.js
@@ -1,10 +1,15 @@
-// Fixing mobile scroll
+/* Fixing mobile scroll
+It's ideal to place this code at the very top of your JavaScript file,so it runs as early as possible when the page loads.
+This script calculates the actual viewport height and sets it as a CSS variable (--vh).
+It fixes the 100vh issue on mobile devices by excluding the browser UI (like the address bar).
+The --vh variable is then used in CSS to apply the correct height.
+This function runs on load and updates on window resize to keep the value accurate.
+*/
 function setVh() {
   let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  document.documentElement.style.setProperty("--vh", vh + "px");
 }
 setVh();
-window.addEventListener('resize', setVh);
-
+window.addEventListener("resize", setVh);
 
 
