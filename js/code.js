1106db0beff74b6795d4fea1531755d81ec465ab
Lubin Mendez <mendezlubin@yahoo.com>
2025-05-18 23:28:12 -0700

    fix: fix mobile scroll issue with CSS and JS

--- js/code.js
+++ js/code.js
@@ -1,4 +1,10 @@
-// alert("conected");
+// Fixing mobile scroll
+function setVh() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+setVh();
+window.addEventListener('resize', setVh);
 
 
 
